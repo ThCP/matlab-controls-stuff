fe43502d12068325cef97d46ef0f405d7e30d9a6
Riccardo <riccardocppzz93@gmail.com>
2015-10-22 13:36:50 +0200

    Moved 2 files, reviewed and added 2 scripts

--- Signal Processing/Test/test_2d_W.m
+++ Signal Processing/Test/test_2d_W.m
@@ -2,20 +2,19 @@
 
 close all, clear all, clc;
 
-N = 250000;
+N = 250000; % number of steps
 
-Ts = 1;
+Ts = 1; % sampling time
 
-n = [0:1:N-1];
+n = [0:1:N-1]; % sampling array
 
-DW = random ('norm', 0, Ts, 2, N);
+DW = random ('norm', 0, Ts, 2, N); % produce an array of randomly distributed variations
 
-W = zeros(2,N);
-
+W = zeros(2,N); % initalization
     
 for i=2:N
-    W(1,i) = W(1,i-1) + DW(1,i);
-    W(2,i) = W(2,i-1) + DW(2,i);
+    W(1,i) = W(1,i-1) + DW(1,i); % variation in the x direction
+    W(2,i) = W(2,i-1) + DW(2,i); % variation in the y direction
 end
 
 plot (W(1,:), W(2,:));
---
+++ personal/matrices_as_images.m
@@ -0,0 +1,8 @@
+% matrici come immagini
+X = randn(100,100);
+imagesc(X)
+colormap(jet)
+figure
+imagesc(X)
+
+colormap(gray)
