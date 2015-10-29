6d808352f616c4ed5e0e2178c9b5223b04fb1bf9
Riccardo <riccardocppzz93@gmail.com>
2015-10-29 15:37:23 +0100

    lab03 signal processing

--- Signal Processing/Test/basic_periodogram.m
+++ Signal Processing/Test/basic_periodogram.m
@@ -2,7 +2,7 @@
 
 clear all, close all, clc;
 
-N = 100000;
+N = 1000000;
 
 n = 0:1:N-1;
 
@@ -16,4 +16,15 @@ P_pos = P(1:N/2+1);
 f = 1/N * (0:N/2);
 
 plot (f, P_pos);
+
+P_sampled = (fft(x_sampled)).^2;
+
+P_sampled_pos = P_sampled(1:N/2);
+
+f_sampled = 1/N * (0:N/2-1);
+
+figure
+plot (f_sampled, P_sampled_pos);
+
+% P_diff = P_pos-P_sampled_pos;
 
