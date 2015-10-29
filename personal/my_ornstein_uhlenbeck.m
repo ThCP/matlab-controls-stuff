5024ab2b6a49466f9fe7188e9c382b591f8c17b5
Riccardo <riccardocppzz93@gmail.com>
2015-10-29 22:31:19 +0100

    fixed orn_uhl and added my_legend

--- personal/my_ornstein_uhlenbeck.m
+++ personal/my_ornstein_uhlenbeck.m
@@ -1,13 +1,16 @@
 %% Generate Ornstein-Uhlenbeck
+function V = my_ornstein_uhlenbeck(N, Ts, beta)
+% The function receives as an input the number of samples, the sampling
+% time and the value of beta and returns an Ornstein-Uhlenbeck process
+% given as the solution of the Langevin equation.
 
 % V[n] = V[n-1]exp(-beta*Ts)+DV[n]
 % V[0] = 0
 % DV[n] ~ N(0,1/(2*beta)*(1-exp(-2*beta*Ts)))
 
-function V = my_ornstein_uhlenbeck(N, Ts, beta)
-n = [0:1:N-1];
+n = 0:1:N-1;
 
-DV = random ('norm', 0, (1/(2*beta)*(1-exp(-2*beta*Ts)))^(1/2), 1, N); %% #################### CONTROLLA
+DV = random ('norm', 0, (1/(2*beta)*(1-exp(-2*beta*Ts)))^(1/2), 1, N); 
 V = zeros(1,N);
 
 V(1) = 0;
@@ -15,4 +18,3 @@ V(1) = 0;
 for i=2:N
     V(i) = V(i-1)*exp(-beta*Ts) + DV(i);
 end
-    
