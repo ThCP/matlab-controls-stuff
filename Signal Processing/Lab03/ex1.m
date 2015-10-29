5115e7413e619dafe1511e9a6f663f435c880287
Riccardo <riccardocppzz93@gmail.com>
2015-10-29 16:08:55 +0100

    lab03 signal processing completed ex 1

--- Signal Processing/Lab03/ex1.m
+++ Signal Processing/Lab03/ex1.m
@@ -61,12 +61,16 @@ W(1) = 0;
     T1(rep) = W(t1);
 end
 
-bins = 50;
+% bins = 50;
+
+bins = min(T0):1:max(T0);
 
 [N1, X1] = hist (T0,bins); % I use the data gathered by the hist funct and I 
                          % use them to plot the estimated pdf 
 figure
 plot(X1, N1); hold on;
+bins = linspace(min(T1), max(T1), (max(T1)-min(T1)));
+% bins = min(T1):1:max(T1);
 [N2, X2] = hist (T1,bins);
 plot(X2, N2);
 legend ('T0', 'T1');
@@ -81,7 +85,7 @@ figure; plot(X1, N1n), hold on;
 
 Y_dist = normpdf(X1, 0, sqrt(t0));
 % return
-Y_dist = Y_dist/sum(Y_dist); % I normalize again to integrate to 1
+% Y_dist = Y_dist/sum(Y_dist); % I normalize again to integrate to 1
 plot (X1, Y_dist);
 
 % at time t1;
@@ -90,8 +94,8 @@ N2n = N2/N2s;
 
 figure; plot(X2, N2n), hold on;
 
-Y_dist = normpdf(X2, 0, sqrt(t1));X
-return
-Y_dist = Y_dist/sum(Y_dist); 
+Y_dist = normpdf(X2, 0, sqrt(t1));
+% return
+% Y_dist = Y_dist/sum(Y_dist); 
 plot (X2, Y_dist);
 
--- Signal Processing/Lab03/ex2.m
+++ Signal Processing/Lab03/ex2.m
@@ -1,7 +1,7 @@
 %% Signal Processing
 % Lab 03
 
-% Exercise 1
+% Exercise 2
 
 close all, clear all,clc;
 
@@ -63,12 +63,14 @@ end
 
 % bins = 50;
 
-bins(
+bins = min(T0):1:max(T0);
 
 [N1, X1] = hist (T0,bins); % I use the data gathered by the hist funct and I 
                          % use them to plot the estimated pdf 
 figure
 plot(X1, N1); hold on;
+% bins = linspace(min(T1), max(T1), (max(T1)-min(T1)));
+bins = min(T1):1:max(T1);
 [N2, X2] = hist (T1,bins);
 plot(X2, N2);
 legend ('T0', 'T1');
@@ -83,7 +85,7 @@ figure; plot(X1, N1n), hold on;
 
 Y_dist = normpdf(X1, 0, sqrt(t0));
 % return
-Y_dist = Y_dist/sum(Y_dist); % I normalize again to integrate to 1
+% Y_dist = Y_dist/sum(Y_dist); % I normalize again to integrate to 1
 plot (X1, Y_dist);
 
 % at time t1;
@@ -93,7 +95,7 @@ N2n = N2/N2s;
 figure; plot(X2, N2n), hold on;
 
 Y_dist = normpdf(X2, 0, sqrt(t1));
-return
-Y_dist = Y_dist/sum(Y_dist); 
+% return
+% Y_dist = Y_dist/sum(Y_dist); 
 plot (X2, Y_dist);
 
