76d2d7ddeb96556201550a63568b07d407149f36
riccardo <riccardocppzz93@gmail.com>
2015-11-19 10:41:39 +0100

    first fixes for sliding functions

--- Signal Processing/Test/sliding_mean.m
+++ Signal Processing/Test/sliding_mean.m
@@ -3,22 +3,29 @@
 % sliding mean window, and returns an array which contains the values of
 % the mean calculated using the given window size.
 
-function [mu] = sliding_mean (X, Nw)
+function [mu_est] = sliding_mean (X, Nw, Istep)
 N = length (X);
-mu = zeros(1,N);
+mu_est = zeros(1,N);
 
-for n=0:N-1
-    n1 = n-Nw/2;
-    n2 = n+Nw/2-1;
-    
+switch nargin
+    case 2
+        ii = 1;
+    case 3
+        ii = Istep;
+end
+
+for n=0:ii:N-1
+    n1 = (n-Nw/2);
+    n2 = n+Nw/2;
+
     if(n1<0)
         n1=0;
     end
-    
+
     if(n2>N-1)
         n2=N-1;
     end
     
-    Xn = X(n1+1:n2+1);
-    mu(n+1) = mean(Xn);
+    Xn = X(n1+1:n2);
+    mu_est(n+1:n+ii)=ones(1,ii)*mean(Xn);  
 end
