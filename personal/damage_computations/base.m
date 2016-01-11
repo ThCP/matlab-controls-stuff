726a2f049f70fa7961089527ed477d4103ed285f
riccardo <riccardocppzz93@gmail.com>
2016-01-11 15:18:36 +0100

    removed damage_computations

--- personal/damage_computations/base.m
+++
@@ -1,9 +0,0 @@
-%% Damage computation v0
-% This script computes the damage inflicted to enemy vehicles depending on
-% the angle and on the side hit. 
-
-close all;
-
-base_damage = 225; % AP shell
-plotByDamage(base_damage);
-
